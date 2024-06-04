68ce3bbda4c5cf80c7f7b6bcd1bc42eda59003e3
Cruelas <irula5198@gmail.com>
2024-06-04 15:16:57 +0700

    commit19

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -7,4 +7,13 @@ int main()
 {
 	cout << "Awal program" << endl;
 	try {
-		
+		array<int, 3> data = { 1,2,3 };
+		cout << data.at(5) << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	cout << "Baris program yang terakhir" << endl;
+
+    return 0;
+}
