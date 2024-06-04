636fee0c89d307599f1d3c3366cffad1141e374f
Cruelas <irula5198@gmail.com>
2024-06-04 15:15:05 +0700

    commit12

--- iosfilewithpath.cpp
+++ iosfilewithpath.cpp
@@ -14,4 +14,17 @@ int main () {
     //menunjuk ke sebuah nama file.
       outfile.open (namafile + "txt", ios::out);
 
-     
+     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    //Unlimited loop untuk menulis
+     while (true) {
+        cout<< "-" ;
+        //Mendapatkan setiap karakter dalam satu baris
+        getline (cin, baris );
+        //Loop akan berhenti jika anda memasukkan karakter q
+        if (baris== "q") break;
+        //Menulis dan memasukkan nila dari baris ke dalam file
+        outfile << baris << endl; 
+        
+     }
+     //selesai dalam menulis sekarang tutup file nya 
+    
